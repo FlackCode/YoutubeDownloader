e9a2ada0ba19112ec08490be12e739af3f559fd6
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-11 19:32:14 +0200

    Turned the script into a console application

--- videoDownloader.py
+++ videoDownloader.py
@@ -1,14 +1,18 @@
 from pytube import YouTube
-from sys import argv
+from getch import pause
 import os
 
 desktopPath = os.path.join(os.path.expanduser('~'), 'Desktop')
-folderName = "Downloaded_Videos"
+folderName = "Downloaded Videos"
 folderPath = os.path.join(desktopPath, folderName)
 os.makedirs(folderPath, exist_ok=True)
-link = argv[1]
-videoLink = YouTube(link)
 
+print('Welcome to Youtube Downloader!')
+link = input('Enter your youtube video link to download it: ')
+ytLink = str(link)
+videoLink = YouTube(ytLink)
+print(f"Downloading \"{videoLink.title}\" to desktop!")
 videoDownload = videoLink.streams.get_highest_resolution()
-
 videoDownload.download(folderPath)
+print("Video successfully downloaded!")
+pause("Press Any Key To Exit.")
